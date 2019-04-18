c9f99e9618b76dbd63abaf3140a3f2639e6cd74d
Ahmed Akbarali <ahmed.amin.akbarali@gmail.com>
2019-04-18 01:30:53 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,9 @@
+
+var address = document.getElementByName("address");
+var submit = document.getElementByName("Submit");
+
+function checkBalance(){
+  var h1 = document.querySelector('h1')
+  h1.style.color = "Green";
+  alert("Hi");
+}
